f90fc92a56330c6c12f972b2f30d4738186396ef
kasumi teruya <all.splashing@gmail.com>
2021-09-28 23:19:14 +0900

    4.2.1 mean squared error

---
+++ ch04/mean_squared_error.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
+y1 = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+print(mean_squared_error(np.array(y1), np.array(t)))
+
+y2 = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+print(mean_squared_error(np.array(y2), np.array(t)))
