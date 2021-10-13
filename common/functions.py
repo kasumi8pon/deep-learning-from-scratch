ed1d704584c70d3c425aef92ca608ac8ae181af2
kasumi teruya <all.splashing@gmail.com>
2021-10-14 00:12:29 +0900

    4.4.2 gradient simplenet

--- common/functions.py
+++ common/functions.py
@@ -10,3 +10,11 @@ def softmax(a):
     y = exp_a / sum_exp_a
 
     return y
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
