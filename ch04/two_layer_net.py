6af7af59f808012dce10a9ba6f90cc95d04e6641
kasumi teruya <all.splashing@gmail.com>
2021-12-25 00:19:18 +0900

    4.5.1 Display params and bias

--- ch04/two_layer_net.py
+++ ch04/two_layer_net.py
@@ -11,8 +11,8 @@ class TwoLayerNet:
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn(input_size, output_size)
-        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
 
     def predict(self, x):
         W1, W2 = self.params['W1'], self.params['W2']
@@ -25,7 +25,7 @@ class TwoLayerNet:
 
         return y
 
-    def loss(self, x, t): # x: 入力データ, y: 教師データ
+    def loss(self, x, t): # x: 入力データ, t: 教師データ
         y = self.predict(x)
 
         return cross_entropy_error(y, t)
@@ -38,13 +38,19 @@ class TwoLayerNet:
         accurancy = np.sum(y == t) / float(x.shape[0])
         return accurancy
 
-    def numerical_gradient(self, x, t): # x: 入力データ, y: 教師データ
-        loss_w = lambda W: self.loss(x, t)
+    def numerical_gradient(self, x, t): # x: 入力データ, t: 教師データ
+        loss_W = lambda W: self.loss(x, t)
 
         grads = {}
-        grads['W1'] = numerical_gradient(loss_w, self.params['W1'])
-        grads['b1'] = numerical_gradient(loss_w, self.params['b1'])
-        grads['W2'] = numerical_gradient(loss_w, self.params['W2'])
-        grads['b2'] = numerical_gradient(loss_w, self.params['b2'])
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
 
         return grads
+
+net = TwoLayerNet(input_size=784, hidden_size=100, output_size=100)
+print(net.params['W1'].shape) # 1層目の重みパラメータ
+print(net.params['b1'].shape) # 1層目の重みバイアス
+print(net.params['W2'].shape) # 2層目の重みパラメータ
+print(net.params['b2'].shape) # 2層目の重みバイアス
