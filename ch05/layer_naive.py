a26a5c636bcf2f73da8e0498f2daee843c6779cc
kasumi teruya <all.splashing@gmail.com>
2022-01-04 18:47:12 +0900

    5.6.3 SoftmaxWithLoss

--- ch05/layer_naive.py
+++ ch05/layer_naive.py
@@ -28,3 +28,11 @@ class AddLayer:
         dx = dout * 1
         dy = dout * 1
         return dx, dy
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 損失
+        self.y = None # softmax の出力
+        self.t = None # 教師データ (one-hot vector)
+
+    def 
--- common/layers.py
+++ common/layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from common.functions import *
 
 class Relu:
     def __init__(self):
@@ -60,5 +61,24 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis = 0)
+
+        return dx
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 損失
+        self.y = None    # softmax の出力
+        self.t = None    # 教師データ (one-hot vector)
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
 
         return dx
