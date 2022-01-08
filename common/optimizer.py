325165f9e2c949ad3b142d0a77cb98add177a305
kasumi teruya <all.splashing@gmail.com>
2022-01-08 20:23:36 +0900

    6.1.5 AdaGrad

--- common/optimizer.py
+++ common/optimizer.py
@@ -24,3 +24,19 @@ class Momentum:
         for key in params.keys():
             self.v[key] = self.momentum*self.v[key] - self.lr*grads[key]
             params[key] += self.v[key]
+
+class AdaGrad:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None:
+          self.h = {}
+          for key, val in params.items():
+              self.h[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key] # 勾配の二乗和
+            # 学習のスケールを調整する。1e-7 を加算して、0 でする除算ことを防ぐ
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key] + 1e-7))s
