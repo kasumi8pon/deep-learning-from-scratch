0d7d64a639cea29dd1ec626a4545fba37e74bd13
kasumi teruya <all.splashing@gmail.com>
2022-01-10 18:30:49 +0900

    6.2.1 optimizer compare mnist

--- common/functions.py
+++ common/functions.py
@@ -12,5 +12,9 @@ def cross_entropy_error(y, t):
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
 
+    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
     batch_size = y.shape[0]
-    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
